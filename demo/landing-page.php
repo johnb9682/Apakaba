8c0c6ecb0e037defcd2e7804598ecbc5b1dc300a
johnb9682 <johnb9682@gmail.com>
2014-12-07 20:55:40 +0800

    Version 1
    Just a templete

---
+++ demo/landing-page.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<!--[if IEMobile 7 ]>    <html class="no-js iem7" lang="en"> <![endif]-->
+<!--[if (gt IEMobile 7)|!(IEMobile)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8"> 
+  
+  <title>Nova is a professional and flexible HTML5 theme by WebMonarchy.com</title>
+  <meta name="description" content="This is nova theme clean and responsive HTML5 theme by WebMonarchy.com. Build a superb looking websites that looks great on desktops, iPads and iPhones! Nova impresses with its beauty and simplicity. Coded in HTML5 & CSS3 - future technology for today and tomorrow. Unique & clean pre made theme skins and easy re-skinning capabilities. ">
+  
+  <!-- The script prevents links from opening in mobile safari. https://gist.github.com/1042026 -->
+  <!-- <script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")</script> -->
+
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/superfish.css" media="screen"  />
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" />
+  <link rel="stylesheet" href="css/skin.css" media="screen"  />
+  <link rel="stylesheet" href="css/custom-rebuild.css" media="screen"  />
+  
+ <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Russo+One' rel='stylesheet' type='text/css'>
+  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+  <script type="text/javascript">	
+
+   function setCookie(c_name,value,exdays)
+		{
+		var exdate=new Date();
+		exdate.setDate(exdate.getDate() + exdays);
+		var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+		document.cookie=c_name + "=" + c_value;
+		}
+		
+	function set_skin(set_skin){
+		setCookie("skinme",set_skin,1);
+	}
+	</script>
+    
+  <link rel="stylesheet" href="css/skins/sweet-pink.css" media="screen">
+    
+
+  
+</head>
+
+<body>
+
+<div id="container">
+    <br /><br />
+        	
+   <section class="page_title_container">
+    <section class="page_title">
+        <h1>This is Nice Landing Page</h1>
+        <h2>Customize this page as you wish</h2>
+    </section><!-- /.page_title -->
+    </section><!-- /.page_title_container -->
+    
+	<section id="content-container">
+    	<section class="content_container_fix">
+            
+             <section class="full_width no_margin">  
+                
+                <section class="single_portfolio_slider" id="single_portfolio_slider">
+                    <div class="slides_container">
+                        <div><img src="images/banner/woman.jpg" width="940" height="499" alt="image" /></div>
+                        <div> <a href="#"><img src="images/banner/flex-banner.jpg" width="940" height="499" alt="image" /></a></div>
+                        <div><img src="images/banner/html5-banner.jpg" width="940" height="499" alt="image" /></div>
+                        <div><a href="#"><img src="images/banner/skins-banner.jpg" width="940" height="499" alt="image" /></a> </div>
+                     </div><!-- /.slides_container -->
+                </section><!-- /.single_portfolio_slider -->
+                
+                <section class="divider_line"></section>
+              
+                <h3>Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis.</h3>
+                 <p>Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            
+                 <section class="divider_line"></section>
+                
+                 <section class="strap_box">
+                    <h3>Nova is a professional and flexible theme to meet your needs</h3>
+                    <p>Build a great looking webiste. This can be used to describe what you do, how you do it.</p>
+                     <a href="#" class="button orange" target="_self"><span>Get a Free Quote</span></a>
+                </section><!-- /.strap_box --> 
+                
+                <section class="one_half">
+                 <h2>Vivamus libero lectus lacinia vel</h2>
+                            
+                <div id="tabs">
+                
+                    <ul>
+                        <li><a href="#tabs-1">Who We Are ?</a></li>
+                        <li><a href="#tabs-2">Our Approach</a></li>
+                        <li><a href="#tabs-3">Our Mission</a></li>
+                    </ul>
+                    
+                    <div id="tabs-1">
+                    <h4>Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h4>
+                     <img src="images/team_man_small.jpg" class="float-left" alt="image"  /> <p>Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <p>Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    
+                    <div id="tabs-2">
+                    <h4>Our Approach</h4>
+                    <p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <p>Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    
+                    <div id="tabs-3">
+                    <h4>Our Mission</h4>
+                    <p>Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>	
+                   
+                </div><!-- /#tabs --> 
+                
+                </section><!-- /.one_half -->
+                
+                <section class="one_half last">
+                      <p>Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                       <p>Mauris porttitor ullamcorper augue. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </section><!-- /.one_half -->
+                 
+             </section><!-- /.full_width -->
+             
+             <section class="clear_full"></section><!-- /.clear_full --> 
+            
+        </section><!-- /.content_container_fix --> 
+    </section><!-- /#content-container -->   
+
+    <footer>
+    	<section class="footer_fix">
+            <section class="footer_meta">
+            	<span class="copyright">All rights reserved. &copy; 2012 NOVA Theme</span>
+            </section><!-- /.footer_meta --> 
+        </section><!-- /.footer_fix --> 
+    </footer><!-- /footer -->
+</div> <!-- /#container -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+  
+  <script type="text/javascript">
+   
+/*-------------------------------
+* jQuery Plugin: Super Fish Menu
+-------------------------------*/
+
+    jQuery(document).ready(function($){ 
+        $("ul.main-menu").supersubs({ 
+            minWidth:    12,   // minimum width of sub-menus in em units 
+            maxWidth:    150,   // maximum width of sub-menus in em units 
+            extraWidth:  1     // extra width can ensure lines don't sometimes turn over 
+                               // due to slight rounding differences and font-family 
+        }).superfish({ 
+            delay:       300,                            // one second delay on mouseout 
+            animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+            speed:       'normal',                          // faster animation speed 
+            autoArrows:  false,                           // disable generation of arrow mark-up 
+            dropShadows: true                            // disable drop shadows 
+        });  // call supersubs first, then superfish, so that subs are 
+                         // not display:none when measuring. Call before initialising 
+                         // containing tabs for same reason. 
+    }); 
+  </script>
+  
+  <script src="js/libs/ui/jquery.ui.core.js"></script>
+  <script src="js/libs/ui/jquery.ui.widget.js"></script>
+  <script src="js/libs/ui/jquery.ui.accordion.js"></script>
+  <script src="js/libs/ui/jquery.ui.tabs.js"></script>
+  <script src="js/libs/superfish/hoverIntent.js"></script> 
+  <script src="js/libs/superfish/superfish.js"></script> 
+  <script src="js/libs/superfish/supersubs.js"></script>
+  <script src="js/libs/jquery.flexslider-min.js"></script>
+  <script type="text/javascript" src="js/libs/jquery.eislideshow.js"></script>
+  <script type="text/javascript" src="js/libs/jquery.easing.1.3.js"></script>
+  <script src="js/libs/slides.min.jquery.js"></script>
+  <script src="js/libs/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+    
+  <script src="js/scripts.js"></script>
+  <!-- scripts concatenated and minified via ant build script-->
+  <script src="js/helper.js"></script>
+  <!-- end scripts-->
+
+</body>
+</html>
